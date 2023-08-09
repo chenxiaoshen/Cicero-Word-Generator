743417e06b8f09a35a8c8531912bfc3cfd341978
Chenxiao Shen <chs224@lehigh.edu>
2023-08-09 17:34:26 -0400

    Added VariableLogger server prototype.

---
+++ VariableLoggerServer/ExampleServerSettings.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures;
+
+namespace Virgil
+{
+    public class ExampleServerSettings : ServerSettingsInterface
+    {
+        private string serverName;
+
+        public string ServerName
+        {
+            get { return serverName; }
+            set { serverName = value; }
+        }
+
+        private string logFilePath;
+
+        public string LogFilePath
+        {
+            get { return logFilePath; }
+            set { logFilePath = value; }
+        }
+    }
+}
